d07617ace242cff470bc3fb511f972c2d52b12d8
antoine-dc <antoinedeconto@gmail.com>
2022-09-28 01:33:13 +0200

    TP2 - Dynamic generation

--- js/script.js
+++ js/script.js
@@ -1,49 +1,65 @@
-// On peut utiliser le await sans être dans une fonction async si notre fichier JS est un module
-const reponse = await fetch("../data/data.json")
-const listPokemon = await reponse.json()
+async function loadData(idGeneration) {
+  // On peut utiliser le await sans être dans une fonction async si notre fichier JS est un module
+  const reponse = await fetch("https://pokebuildapi.fr/api/v1/pokemon/generation/" + idGeneration)
+  const listPokemon = await reponse.json()
 
-for (const pokemon of listPokemon) {
-  let article = document.createElement("article")
+  for (const pokemon of listPokemon) {
+    let main = document.querySelector("main")
+    let article = document.createElement("article")
 
-  article.innerHTML = `
-        <figure>
-          <picture>
-            <img src="${pokemon.image}" alt="Image ${pokemon.name}" />
-          </picture>
-          <figcaption>
-            <span class="types">${pokemon.apiTypes[0].name}</span>
-            <h2>Bulbizarre</h2>
-            <ol>
-              <li>Points de vie : ${pokemon.stats.HP}</li>
-              <li>Attaque : ${pokemon.stats.attack}</li>
-              <li>Défense : ${pokemon.stats.defense}</li>
-              <li>Attaque spécial : ${pokemon.stats.special_attack}</li>
-              <li>Vitesse : ${pokemon.stats.speed}</li>
-            </ol>
-          </figcaption>
-        </figure>`
+    article.innerHTML = `
+                <figure>
+                <picture>
+                    <img src="${pokemon.image}" alt="Image ${pokemon.name}" />
+                </picture>
+                <figcaption>
+                    <span class="types">${pokemon.apiTypes[0].name}</span>
+                    <h2>${pokemon.name}</h2>
+                    <ol>
+                    <li>Points de vie : ${pokemon.stats.HP}</li>
+                    <li>Attaque : ${pokemon.stats.attack}</li>
+                    <li>Défense : ${pokemon.stats.defense}</li>
+                    <li>Attaque spécial : ${pokemon.stats.special_attack}</li>
+                    <li>Vitesse : ${pokemon.stats.speed}</li>
+                    </ol>
+                </figcaption>
+                </figure>`
 
-  let color
-  switch (pokemon.apiTypes[0].name) {
-    case "Plante":
-      color = "green"
-      break
-    case "Feu":
-      color = "orange"
-      break
+    let color
+    switch (pokemon.apiTypes[0].name) {
+      case "Plante":
+        color = "green"
+        break
+      case "Feu":
+        color = "orange"
+        break
 
-    case "Eau":
-      color = "blue"
-      break
+      case "Eau":
+        color = "blue"
+        break
 
-    default:
-      color = "grey"
-      break
+      default:
+        color = "grey"
+        break
+    }
+
+    article.style.borderColor = color
+    article.style.backgroundColor = color
+
+    main.appendChild(article)
   }
-
-  article.style.borderColor = color
-  article.style.backgroundColor = color
-
-  let main = document.querySelector("main")
-  main.appendChild(article)
 }
+
+const selectElem = document.querySelector("select")
+
+// When a new <option> is selected
+selectElem.addEventListener("change", () => {
+  const index = selectElem.value
+  let main = document.querySelector("main")
+
+  // On reset le main
+  main.innerHTML = ""
+  loadData(index)
+})
+
+loadData(1)
